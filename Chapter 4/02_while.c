718302ecd258361f6069a8c3459eda941ec7ea6e
Swaroop-bit <swaroopsinghbhati88@gmail.com>
2024-09-19 16:36:51 +0530

    j

--- Chapter 4/02_while.c
+++ Chapter 4/02_while.c
@@ -2,10 +2,10 @@
 
 int main(){
 
-    int i = 3;
+    int i = 0;
     while(i<4){
         printf("Happy Birthday!\n");
-        i = i + 1;
+        i = i + 1;// it can also be written as i++
     }
 
     return 0;
