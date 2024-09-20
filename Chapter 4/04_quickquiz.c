3a823aee3d2b8de02c0a10e8076a2ea455532c0c
Swaroop-bit <swaroopsinghbhati88@gmail.com>
2024-09-20 11:16:04 +0530

    Hey

--- Chapter 4/04_quickquiz.c
+++ Chapter 4/04_quickquiz.c
@@ -4,10 +4,11 @@ int main(){
 
     int i = 0;
     while(i<=20){
-        if(i>10){
+        if(i>=10){
             printf("The value of i is %d\n",i);
         }
-    }i++;
+        i++;
+    }
 
     return 0;
 }
---
+++ Chapter 4/06_increment_and_decrement.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+     int i =5;
+     i = i + 5;
+     primtf("The value of i is%d\n",i);
+
+    return 0;
+}
