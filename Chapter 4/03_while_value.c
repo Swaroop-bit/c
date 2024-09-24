dbb259ebd032c5a83e32d0ed01b838ee07d7fd49
Swaroop-bit <swaroopsinghbhati88@gmail.com>
2024-09-25 01:29:26 +0530

    jk

--- Chapter 4/03_while_value.c
+++ Chapter 4/03_while_value.c
@@ -3,9 +3,9 @@
 int main(){
 
     int i =0;
-    while(i<10){
+    while(i<=10){
         printf("The value of i is %d \n",i);
-        i++;
+        i++;// i = i + 1
     }// the momment the value of i becomes 10 the loop terminates
     
   
